ef89b897cc5c2b23e8224a81a8ba21acbc2d4d37
whitedavidgoogle <whitedavid@google.com>
2017-03-23 14:45:29 -0400

    populating arrays

--- pysearch.py
+++ pysearch.py
@@ -1,16 +1,25 @@
+#Vars
+i = 0
+a = 0
 dslistbool = []
 dslistssn = []
 dirarray =[]
-de =scandir.scadir(sys.argv[1])
-while 1:
-  	try:
-      	d = de.next()
-        dirarray.append = d.path
-	except StopIterantion as _:
-      	break
-while i < len(dirarray)
-	searchfile = open(dirarray[i])
-		for line in searchfile:
-        	if "HONORABLE" or "Honorable" in line: dslistbool.append = "eligible:true"
-	searchfile.close()
+import os
+#Loops
+#populate dirarray
+for root,dirs, files in os.walk(r'.'):
+    for file in files:
+        if file.endswith('.txt'):
+            dirarray.append(file)
 
+while a < len(dirarray):
+        print dirarray[a]
+        a=a+1
+#populate bool array
+while i < len(dirarray):
+
+        searchfile = open(dirarray[i])
+        for line in searchfile:
+                if "HONORABLE" or "Honorable" in line: dslistbool.append("eligible:true")
+        searchfile.close()
+        i=i+1
