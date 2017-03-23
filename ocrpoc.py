9ce59a23d3bdb643dd5bbc79de9b8d2ff5b25a11
whitedavidgoogle <whitedavid@google.com>
2017-03-23 13:41:14 -0400

    updated Readme with pcode

--- ocrpoc.py
+++ ocrpoc.py
@@ -3,8 +3,10 @@ from google.cloud import datastore
 def create_client(project_id):
     return datastore.Client(project_id)
 
-def add(discharge, HONORABLE):
-    key = discharge.key(
+def add(client, description):
+	key = client.key('discharge')
+	discharge.update({
+	
 
 String result = false
 
@@ -13,22 +15,17 @@ filename = raw_input()
 
 
 
-def transfer_funds(client, from_key, to_key, amount):
-    with client.transaction():
-        from_account = client.get(from_key)
-        to_account = client.get(to_key)
+#def transfer_funds(client, from_key, to_key, amount):
+#    with client.transaction():
+#        from_account = client.get(from_key)
+#        to_account = client.get(to_key)#
 
-        from_account['balance'] -= amount
-        to_account['balance'] += amount
+ #       from_account['balance'] -= amount
+#        to_account['balance'] += amount
 
-        client.put_multi([from_account, to_account])
-
+  #      client.put_multi([from_account, to_account])
+#search the file
 searchfile = open(filename)
 for line in searchfile:
         if "HONORABLE" or "Honorable" in line: print "yes" and result = true
 searchfile.close()
-
-#populate an array to write filename and result
-
-#def dbwrite()
-        #insert datastore write here	
