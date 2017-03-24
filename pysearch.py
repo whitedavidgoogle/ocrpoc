f851e7f3e20b757cb01f58e5892160c1dcf49144
whitedavidgoogle <whitedavid@google.com>
2017-03-24 15:31:54 +0000

    changed txt dir

--- pysearch.py
+++ pysearch.py
@@ -1,32 +1,33 @@
 #Vars
 i = 0
 a = 0
+b = 0
 dslistbool = []
 dslistssn = []
 dirarray =[]
 posresult = "true"
 import os
 #Loops
+
 #populate dirarray
-for root,dirs, files in os.walk(r'ocrpoc/data/'):
+for root,dirs, files in os.walk(r'.'):
     for file in files:
         if file.endswith('.txt'):
             dirarray.append(file)
 
-while a < len(dirarray):
-        print dirarray[a]
-#	print dslistbool[a]
-	a=a+1
+
 #populate bool array
 while i < len(dirarray):
 	searchfile = open(dirarray[i])
         for line in searchfile:
-		print i
+#		print dirarray[i]
                	if "HONORABLE" or "Honorable" in line: 
 			dslistbool.append(posresult)
 		i=i+1
         searchfile.close()
         #i=i+1
+
+
 #regex for finding ssn
 #def ssnfinder():
 
